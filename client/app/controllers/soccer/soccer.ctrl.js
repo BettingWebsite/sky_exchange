22d89b686d8208bfeea3d10dda481d9939b4ec98
Winner <73909926+winner108@users.noreply.github.com>
2022-01-07 22:10:48 +0800

    update

--- client/app/controllers/soccer/soccer.ctrl.js
+++ client/app/controllers/soccer/soccer.ctrl.js
@@ -1,3 +1,262 @@
+const SoccerMocData = {
+  "data": [
+    {
+      "marketCount": 14,
+      "event": {
+        "open_date": "Fri Jan 07 10:03:28 GMT 2022",
+        "sport_id": "1",
+        "sport_name": "Football",
+        "timezone": "GMT",
+        "countryCode": "LK",
+        "match_id": "31161595",
+        "series_name": "Sri Lankan Matches",
+        "match_name": "Renown SC v Up Country Lions SC",
+        "series_id": "12083220"
+      }
+    },
+    {
+      "marketCount": 14,
+      "event": {
+        "open_date": "Fri Jan 07 10:29:49 GMT 2022",
+        "sport_id": "1",
+        "sport_name": "Football",
+        "timezone": "GMT",
+        "countryCode": "IL",
+        "match_id": "31163067",
+        "series_name": "Israeli Liga Bet",
+        "match_name": "Beitar Kfar Saba Shlomi v Beitar Petah Tikva",
+        "series_id": "12201049"
+      }
+    },
+    {
+      "marketCount": 19,
+      "event": {
+        "open_date": "Fri Jan 07 11:00:06 GMT 2022",
+        "sport_id": "1",
+        "sport_name": "Football",
+        "timezone": "GMT",
+        "countryCode": "NP",
+        "match_id": "31161658",
+        "series_name": "Nepalese Matches",
+        "match_name": "Armed Police Force v Jawalakhel Youth Club",
+        "series_id": "12277166"
+      }
+    },
+    {
+      "marketCount": 14,
+      "event": {
+        "open_date": "Fri Jan 07 10:02:32 GMT 2022",
+        "sport_id": "1",
+        "sport_name": "Football",
+        "timezone": "GMT",
+        "countryCode": "LK",
+        "match_id": "31161593",
+        "series_name": "Sri Lankan Matches",
+        "match_name": "Ratnam SC v Sea Hawks FC",
+        "series_id": "12083220"
+      }
+    },
+    {
+      "marketCount": 14,
+      "event": {
+        "open_date": "Fri Jan 07 10:02:36 GMT 2022",
+        "sport_id": "1",
+        "sport_name": "Football",
+        "timezone": "GMT",
+        "countryCode": "IL",
+        "match_id": "31163064",
+        "series_name": "Israeli Liga Bet",
+        "match_name": "Beitar Ironi Kiryat Gat v Maccabi Kiryat Malakhi",
+        "series_id": "12201049"
+      }
+    },
+    {
+      "marketCount": 13,
+      "event": {
+        "open_date": "Fri Jan 07 10:32:10 GMT 2022",
+        "sport_id": "1",
+        "sport_name": "Football",
+        "timezone": "GMT",
+        "countryCode": "IL",
+        "match_id": "31161503",
+        "series_name": "Israeli Liga Bet",
+        "match_name": "Maccabi Beer Sheva v MS Hapoel Lod",
+        "series_id": "12201049"
+      }
+    },
+    {
+      "marketCount": 19,
+      "event": {
+        "open_date": "Fri Jan 07 11:15:00 GMT 2022",
+        "sport_id": "1",
+        "sport_name": "Football",
+        "timezone": "GMT",
+        "countryCode": "IL",
+        "match_id": "31161872",
+        "series_name": "Israeli Liga Alef",
+        "match_name": "Ironi Kuseife v Hapoel Marmorek",
+        "series_id": "5618479"
+      }
+    },
+    {
+      "marketCount": 17,
+      "event": {
+        "open_date": "Fri Jan 07 11:16:23 GMT 2022",
+        "sport_id": "1",
+        "sport_name": "Football",
+        "timezone": "GMT",
+        "countryCode": "IL",
+        "match_id": "31162064",
+        "series_name": "Israeli Liga Alef",
+        "match_name": "Hapoel Migdal Haemek v MS Tira",
+        "series_id": "5618479"
+      }
+    },
+    {
+      "marketCount": 12,
+      "event": {
+        "open_date": "Fri Jan 07 11:29:38 GMT 2022",
+        "sport_id": "1",
+        "sport_name": "Football",
+        "timezone": "GMT",
+        "countryCode": "IL",
+        "match_id": "31163062",
+        "series_name": "Israeli Liga Bet",
+        "match_name": "MS Bnie Jaffa Ortodoxim v Sporting Tel Aviv",
+        "series_id": "12201049"
+      }
+    },
+    {
+      "marketCount": 15,
+      "event": {
+        "open_date": "Fri Jan 07 09:59:13 GMT 2022",
+        "sport_id": "1",
+        "sport_name": "Football",
+        "timezone": "GMT",
+        "countryCode": "IL",
+        "match_id": "31160395",
+        "series_name": "Israeli Liga Alef",
+        "match_name": "Hapoel Iksal Imad v Ironi Nesher",
+        "series_id": "5618479"
+      }
+    },
+    {
+      "marketCount": 8,
+      "event": {
+        "open_date": "Fri Jan 07 09:59:17 GMT 2022",
+        "sport_id": "1",
+        "sport_name": "Football",
+        "timezone": "GMT",
+        "countryCode": "IL",
+        "match_id": "31160394",
+        "series_name": "Israeli Liga Alef",
+        "match_name": "AS Nordia Jerusalem v Shimshon Tel Aviv",
+        "series_id": "5618479"
+      }
+    },
+    {
+      "marketCount": 11,
+      "event": {
+        "open_date": "Fri Jan 07 09:59:39 GMT 2022",
+        "sport_id": "1",
+        "sport_name": "Football",
+        "timezone": "GMT",
+        "countryCode": "IL",
+        "match_id": "31160393",
+        "series_name": "Israeli Liga Alef",
+        "match_name": "Hapoel Bikat Hayarden v Hapoel Azor",
+        "series_id": "5618479"
+      }
+    },
+    {
+      "marketCount": 13,
+      "event": {
+        "open_date": "Fri Jan 07 10:44:56 GMT 2022",
+        "sport_id": "1",
+        "sport_name": "Football",
+        "timezone": "GMT",
+        "countryCode": "IL",
+        "match_id": "31162063",
+        "series_name": "Israeli Liga Alef",
+        "match_name": "Maccabi Herzliya v Maccabi Kabilio Jaffa",
+        "series_id": "5618479"
+      }
+    },
+    {
+      "marketCount": 19,
+      "event": {
+        "open_date": "Fri Jan 07 11:00:35 GMT 2022",
+        "sport_id": "1",
+        "sport_name": "Football",
+        "timezone": "GMT",
+        "countryCode": "IL",
+        "match_id": "31162286",
+        "series_name": "Israeli Liga Bet",
+        "match_name": "Ironi Beit Dagan v Hapoel Mahane Yehuda",
+        "series_id": "12201049"
+      }
+    },
+    {
+      "marketCount": 15,
+      "event": {
+        "open_date": "Fri Jan 07 10:53:12 GMT 2022",
+        "sport_id": "1",
+        "sport_name": "Football",
+        "timezone": "GMT",
+        "countryCode": "IL",
+        "match_id": "31162061",
+        "series_name": "Israeli Liga Alef",
+        "match_name": "Maccabi Yavne v Shimshon Kfar Qasem",
+        "series_id": "5618479"
+      }
+    },
+    {
+      "marketCount": 10,
+      "event": {
+        "open_date": "Fri Jan 07 10:30:19 GMT 2022",
+        "sport_id": "1",
+        "sport_name": "Football",
+        "timezone": "GMT",
+        "countryCode": "IL",
+        "match_id": "31162060",
+        "series_name": "Israeli Liga Alef",
+        "match_name": "Maccabi Shaaraim v SC Dimona",
+        "series_id": "5618479"
+      }
+    },
+    {
+      "marketCount": 15,
+      "event": {
+        "open_date": "Fri Jan 07 11:22:38 GMT 2022",
+        "sport_id": "1",
+        "sport_name": "Football",
+        "timezone": "GMT",
+        "countryCode": "IL",
+        "match_id": "31161676",
+        "series_name": "Israeli U19",
+        "match_name": "Hapoel Petach Tikva U19 v Maccabi Yavne U19",
+        "series_id": "11709225"
+      }
+    },
+    {
+      "marketCount": 19,
+      "event": {
+        "open_date": "Fri Jan 07 11:08:54 GMT 2022",
+        "sport_id": "1",
+        "sport_name": "Football",
+        "timezone": "GMT",
+        "countryCode": "IL",
+        "match_id": "31161504",
+        "series_name": "Israeli Liga Alef",
+        "match_name": "Hapoel Tzafririm Holon v FC Hapoel Ashkelon",
+        "series_id": "5618479"
+      }
+    }
+  ],
+  "success": true,
+  "message": "Match List"
+}
+
 app.controller(
   "soccerController",
   function ($scope, $http, $cookieStore, $interval, $rootScope) {
@@ -18,6 +277,7 @@ app.controller(
     $scope.oldHighlightlist = [];
     $scope.showsoccerinter = false;
     $rootScope.s_id = "1";
+    $rootScope.fType = 1
     $scope.showsoccer = function () {
       $rootScope.getMatchDataFromWebSocket();
       $(".btn-lay").removeClass("spark");
@@ -26,88 +286,89 @@ app.controller(
         $scope.showsoccerinter = true;
       }
       if ($rootScope.fType == 1) {
-        $http({
-          url:
-            "http://www.lcexchanges247.com/Client/BetClient.svc/Data/Highlights?sid=" +
-            1,
+        // $http({
+        //   url:
+        //     "http://www.lcexchanges247.com/Client/BetClient.svc/Data/Highlights?sid=" +
+        //     1,
 
-          method: "GET",
+        //   method: "GET",
 
-          headers: {
-            Token: authtoken,
-          },
-        }).then(
-          function mySuccess(response) {
-            $("#loading_page").css("display", "none");
-            $scope.showsoccerinter = false;
-            // console.log(response);
-            // $scope.cricket = response.data.data;
-            if (response.data.data.length < 1) {
-              $scope.shoNoEvents = true;
-            } else {
-              $scope.shoNoEvents = false;
+        //   headers: {
+        //     Token: authtoken,
+        //   },
+        // }).then(
+        // function mySuccess(response) {
+        $("#loading_page").css("display", "none");
+        $scope.showsoccerinter = false;
+        // console.log(response);
+        // $scope.cricket = response.data.data;
+
+        if (SoccerMocData.data.length < 1) {
+          $scope.shoNoEvents = true;
+        } else {
+          $scope.shoNoEvents = false;
+        }
+        $scope.Highlightlist = SoccerMocData.data;
+
+        if ($scope.oldHighlightlist.length != $scope.Highlightlist.length) {
+          $scope.oldHighlightlist = $scope.Highlightlist;
+        }
+
+        angular.forEach($scope.Highlightlist, function (item, index) {
+          if (item.matchName == $scope.oldHighlightlist[index].matchName) {
+            if (
+              item.runner1Back != $scope.oldHighlightlist[index].runner1Back
+            ) {
+              $("#hback1_" + index).addClass("spark");
             }
-            $scope.Highlightlist = response.data.data;
-
-            if ($scope.oldHighlightlist.length != $scope.Highlightlist.length) {
-              $scope.oldHighlightlist = $scope.Highlightlist;
+            if (
+              item.runner1Lay != $scope.oldHighlightlist[index].runner1Lay
+            ) {
+              $("#hlay1_" + index).addClass("spark");
             }
-
-            angular.forEach($scope.Highlightlist, function (item, index) {
-              if (item.matchName == $scope.oldHighlightlist[index].matchName) {
-                if (
-                  item.runner1Back != $scope.oldHighlightlist[index].runner1Back
-                ) {
-                  $("#hback1_" + index).addClass("spark");
-                }
-                if (
-                  item.runner1Lay != $scope.oldHighlightlist[index].runner1Lay
-                ) {
-                  $("#hlay1_" + index).addClass("spark");
-                }
-                if (
-                  item.runner2Back != $scope.oldHighlightlist[index].runner2Back
-                ) {
-                  $("#hback2_" + index).addClass("spark");
-                }
-                if (
-                  item.runner2Lay != $scope.oldHighlightlist[index].runner2Lay
-                ) {
-                  $("#hlay2_" + index).addClass("spark");
-                }
-                if (
-                  item.runner3Back != $scope.oldHighlightlist[index].runner3Back
-                ) {
-                  $("#hback3_" + index).addClass("spark");
-                }
-                if (
-                  item.runner3Lay != $scope.oldHighlightlist[index].runner3Lay
-                ) {
-                  $("#hlay3_" + index).addClass("spark");
-                }
-                if (item.bfId == $scope.oldHighlightlist[index].bfId) {
-                  // console.log(item.bfId)
-                }
-                if (item.marketId == $scope.oldHighlightlist[index].marketId) {
-                  // console.log(item.marketId)
-                }
-                if (item.mtBfId == $scope.oldHighlightlist[index].mtBfId) {
-                  // console.log(item.mtBfId)
-                }
-              }
-            });
-            $scope.oldHighlightlist = $scope.Highlightlist;
-
-            // console.log($scope.cricket)
-          },
-          function myError(error) {
-            $scope.showsoccerinter = false;
-            if (error.status == 401) {
-              // $.removeCookie("authtoken");
-              // window.location.href="index.html"
+            if (
+              item.runner2Back != $scope.oldHighlightlist[index].runner2Back
+            ) {
+              $("#hback2_" + index).addClass("spark");
+            }
+            if (
+              item.runner2Lay != $scope.oldHighlightlist[index].runner2Lay
+            ) {
+              $("#hlay2_" + index).addClass("spark");
+            }
+            if (
+              item.runner3Back != $scope.oldHighlightlist[index].runner3Back
+            ) {
+              $("#hback3_" + index).addClass("spark");
+            }
+            if (
+              item.runner3Lay != $scope.oldHighlightlist[index].runner3Lay
+            ) {
+              $("#hlay3_" + index).addClass("spark");
+            }
+            if (item.bfId == $scope.oldHighlightlist[index].bfId) {
+              // console.log(item.bfId)
+            }
+            if (item.marketId == $scope.oldHighlightlist[index].marketId) {
+              // console.log(item.marketId)
+            }
+            if (item.mtBfId == $scope.oldHighlightlist[index].mtBfId) {
+              // console.log(item.mtBfId)
             }
           }
-        );
+        });
+        $scope.oldHighlightlist = $scope.Highlightlist;
+
+        // console.log($scope.cricket)
+        // },
+        //   function myError(error) {
+        //     $scope.showsoccerinter = false;
+        //     if (error.status == 401) {
+        //       // $.removeCookie("authtoken");
+        //       // window.location.href="index.html"
+        //     }
+        //   }
+        // );
       } else {
         $("#loading_page").css("display", "none");
         $scope.showsoccerinter = false;
--- client/app/controllers/tennis/tennis.ctrl.js
+++ client/app/controllers/tennis/tennis.ctrl.js
@@ -1,3 +1,246 @@
+const tennisMocData = {
+  "data": [
+    {
+      "marketCount": 13,
+      "event": {
+        "open_date": "Fri Jan 07 10:44:00 GMT 2022",
+        "sport_id": "2",
+        "sport_name": "Tennis",
+        "timezone": "UTC",
+        "countryCode": "AU",
+        "match_id": "31160569",
+        "series_name": "ATP Adelaide 2022",
+        "match_name": "Set 03",
+        "series_id": "12437605"
+      }
+    },
+    {
+      "marketCount": 8,
+      "event": {
+        "open_date": "Fri Jan 07 10:44:00 GMT 2022",
+        "sport_id": "2",
+        "sport_name": "Tennis",
+        "timezone": "UTC",
+        "countryCode": "AU",
+        "match_id": "31160568",
+        "series_name": "ATP Adelaide 2022",
+        "match_name": "Set 02",
+        "series_id": "12437605"
+      }
+    },
+    {
+      "marketCount": 3,
+      "event": {
+        "open_date": "Fri Jan 07 14:30:00 GMT 2022",
+        "sport_id": "2",
+        "sport_name": "Tennis",
+        "timezone": "UTC",
+        "countryCode": "US",
+        "match_id": "31154109",
+        "series_name": "UTR Naples Men's 2022",
+        "match_name": "Velasquez Ariza v Ale Moreno",
+        "series_id": "12437977"
+      }
+    },
+    {
+      "marketCount": 1,
+      "event": {
+        "open_date": "Fri Jan 07 12:03:05 GMT 2022",
+        "sport_id": "2",
+        "sport_name": "Tennis",
+        "timezone": "GMT",
+        "countryCode": "GB",
+        "match_id": "31162867",
+        "series_name": "ITF Egypt Futures",
+        "match_name": "B Lock v D Klok",
+        "series_id": "9663755"
+      }
+    },
+    {
+      "marketCount": 1,
+      "event": {
+        "open_date": "Fri Jan 07 12:05:07 GMT 2022",
+        "sport_id": "2",
+        "sport_name": "Tennis",
+        "timezone": "GMT",
+        "countryCode": "GB",
+        "match_id": "31162866",
+        "series_name": "ITF Egypt Futures",
+        "match_name": "M Beauge v S Roncalli",
+        "series_id": "9663755"
+      }
+    },
+    {
+      "marketCount": 3,
+      "event": {
+        "open_date": "Fri Jan 07 02:35:00 GMT 2022",
+        "sport_id": "2",
+        "sport_name": "Tennis",
+        "timezone": "UTC",
+        "match_id": "31161201",
+        "series_name": "WTA Melbourne Summer Set 1 2022",
+        "match_name": "Set 03",
+        "series_id": "12438662"
+      }
+    },
+    {
+      "marketCount": 1,
+      "event": {
+        "open_date": "Fri Jan 07 11:12:22 GMT 2022",
+        "sport_id": "2",
+        "sport_name": "Tennis",
+        "timezone": "GMT",
+        "countryCode": "GB",
+        "match_id": "31162961",
+        "series_name": "ITF Spain Futures",
+        "match_name": "A Bellier v D Rincon",
+        "series_id": "9160746"
+      }
+    },
+    {
+      "marketCount": 7,
+      "event": {
+        "open_date": "Fri Jan 07 10:44:00 GMT 2022",
+        "sport_id": "2",
+        "sport_name": "Tennis",
+        "timezone": "UTC",
+        "countryCode": "AU",
+        "match_id": "31160565",
+        "series_name": "ATP Adelaide 2022",
+        "match_name": "Kokkinakis v M Ymer",
+        "series_id": "12437605"
+      }
+    },
+    {
+      "marketCount": 1,
+      "event": {
+        "open_date": "Fri Jan 07 10:22:37 GMT 2022",
+        "sport_id": "2",
+        "sport_name": "Tennis",
+        "timezone": "GMT",
+        "countryCode": "GB",
+        "match_id": "31162804",
+        "series_name": "ITF Cairo",
+        "match_name": "M Tkacheva v R Sawayanagi",
+        "series_id": "10479523"
+      }
+    },
+    {
+      "marketCount": 1,
+      "event": {
+        "open_date": "Fri Jan 07 11:22:45 GMT 2022",
+        "sport_id": "2",
+        "sport_name": "Tennis",
+        "timezone": "GMT",
+        "countryCode": "GB",
+        "match_id": "31162868",
+        "series_name": "ITF Cairo",
+        "match_name": "L Tran v A Ureke",
+        "series_id": "10479523"
+      }
+    },
+    {
+      "marketCount": 3,
+      "event": {
+        "open_date": "Fri Jan 07 10:47:00 GMT 2022",
+        "sport_id": "2",
+        "sport_name": "Tennis",
+        "timezone": "UTC",
+        "countryCode": "IT",
+        "match_id": "31162186",
+        "series_name": "Forli Challenger 2022",
+        "match_name": "Benchetrit v M Sasikumar",
+        "series_id": "12437753"
+      }
+    },
+    {
+      "marketCount": 4,
+      "event": {
+        "open_date": "Fri Jan 07 02:35:00 GMT 2022",
+        "sport_id": "2",
+        "sport_name": "Tennis",
+        "timezone": "UTC",
+        "match_id": "31161199",
+        "series_name": "WTA Melbourne Summer Set 1 2022",
+        "match_name": "Set 01",
+        "series_id": "12438662"
+      }
+    },
+    {
+      "marketCount": 13,
+      "event": {
+        "open_date": "Fri Jan 07 10:47:00 GMT 2022",
+        "sport_id": "2",
+        "sport_name": "Tennis",
+        "timezone": "UTC",
+        "countryCode": "IT",
+        "match_id": "31162190",
+        "series_name": "Forli Challenger 2022",
+        "match_name": "Set 03",
+        "series_id": "12437753"
+      }
+    },
+    {
+      "marketCount": 4,
+      "event": {
+        "open_date": "Fri Jan 07 10:47:00 GMT 2022",
+        "sport_id": "2",
+        "sport_name": "Tennis",
+        "timezone": "UTC",
+        "countryCode": "IT",
+        "match_id": "31162189",
+        "series_name": "Forli Challenger 2022",
+        "match_name": "Set 02",
+        "series_id": "12437753"
+      }
+    },
+    {
+      "marketCount": 1,
+      "event": {
+        "open_date": "Fri Jan 07 10:40:19 GMT 2022",
+        "sport_id": "2",
+        "sport_name": "Tennis",
+        "timezone": "GMT",
+        "countryCode": "GB",
+        "match_id": "31162880",
+        "series_name": "ITF Tunisia Futures",
+        "match_name": "T Deschamps v R Balzerani",
+        "series_id": "9245107"
+      }
+    },
+    {
+      "marketCount": 1,
+      "event": {
+        "open_date": "Fri Jan 07 07:41:00 GMT 2022",
+        "sport_id": "2",
+        "sport_name": "Tennis",
+        "timezone": "UTC",
+        "countryCode": "AE",
+        "match_id": "31156326",
+        "series_name": "UTR Dubai Men's II 2022",
+        "match_name": "Podzus v Sim Roberts",
+        "series_id": "12438463"
+      }
+    },
+    {
+      "marketCount": 2,
+      "event": {
+        "open_date": "Fri Jan 07 11:15:00 GMT 2022",
+        "sport_id": "2",
+        "sport_name": "Tennis",
+        "timezone": "Australia/Sydney",
+        "countryCode": "AU",
+        "match_id": "31163525",
+        "series_name": "ATP Cup 2022",
+        "match_name": "Martinez/Ramos Vinolas v Walkow/Zielinski",
+        "series_id": "12437191"
+      }
+    }
+  ],
+  "success": true,
+  "message": "Match List"
+}
+
 app.controller(
   "tennisController",
   function ($scope, $http, $cookieStore, $interval, $rootScope) {
@@ -14,6 +257,7 @@ app.controller(
 
     $scope.fullScore = {};
     $rootScope.s_id = "2";
+    $rootScope.fType = 1
     $scope.Highlightlist = [];
     $scope.oldHighlightlist = [];
     $("#loading_page").css("display", "grid");
@@ -25,84 +269,89 @@ app.controller(
       if ($scope.showtennisinter == false) {
         $scope.showtennisinter = true;
       }
+
+      console.log($rootScope.fType, "$rootScope.fType")
+
       if ($rootScope.fType == 1) {
-        $http({
-          url:
-            "http://www.lcexchanges247.com/Client/BetClient.svc/Data/Highlights?sid=2",
+        // $http({
+        //   url:
+        //     "http://www.lcexchanges247.com/Client/BetClient.svc/Data/Highlights?sid=2",
 
-          method: "GET",
+        //   method: "GET",
 
-          headers: {
-            Token: authtoken,
-          },
-        }).then(
-          function mySuccess(response) {
-            $scope.showtennisinter = false;
-            // console.log(response);
-            // $scope.cricket = response.data.data;
-            if (response.data.data.length < 1) {
-              $scope.shoNoEvents = true;
-            } else {
-              $scope.shoNoEvents = false;
-            }
-            $scope.Highlightlist = response.data.data;
-
-            $("#loading_page").css("display", "none");
-
-            if ($scope.oldHighlightlist.length != $scope.Highlightlist.length) {
-              $scope.oldHighlightlist = $scope.Highlightlist;
-            }
-
-            angular.forEach($scope.Highlightlist, function (item, index) {
-              if (item.matchName == $scope.oldHighlightlist[index].matchName) {
-                if (
-                  item.runner1Back != $scope.oldHighlightlist[index].runner1Back
-                ) {
-                  $("#hback1_" + index).addClass("spark");
-                }
-                if (
-                  item.runner1Lay != $scope.oldHighlightlist[index].runner1Lay
-                ) {
-                  $("#hlay1_" + index).addClass("spark");
-                }
-                if (
-                  item.runner2Back != $scope.oldHighlightlist[index].runner2Back
-                ) {
-                  $("#hback2_" + index).addClass("spark");
-                }
-                if (
-                  item.runner2Lay != $scope.oldHighlightlist[index].runner2Lay
-                ) {
-                  $("#hlay2_" + index).addClass("spark");
-                }
-                if (
-                  item.runner3Back != $scope.oldHighlightlist[index].runner3Back
-                ) {
-                  $("#hback3_" + index).addClass("spark");
-                }
-                if (
-                  item.runner3Lay != $scope.oldHighlightlist[index].runner3Lay
-                ) {
-                  $("#hlay3_" + index).addClass("spark");
-                }
-              }
-            });
-            $scope.oldHighlightlist = $scope.Highlightlist;
-
-            // console.log($scope.cricket)
-          },
-          function myError(error) {
-            $scope.showtennisinter = false;
-            if (error.status == 401) {
-              // $.removeCookie("authtoken");
-              // window.location.href="index.html"
-            }
-          }
-        );
-      } else {
+        //   headers: {
+        //     Token: authtoken,
+        //   },
+        // }).then(
+        // function mySuccess(response) {
+        console.log("success")
         $scope.showtennisinter = false;
         // console.log(response);
         // $scope.cricket = response.data.data;
+        if (tennisMocData.data.length < 1) {
+          $scope.shoNoEvents = true;
+        } else {
+          $scope.shoNoEvents = false;
+        }
+        $scope.Highlightlist = tennisMocData.data;
+
+        $("#loading_page").css("display", "none");
+
+        if ($scope.oldHighlightlist.length != $scope.Highlightlist.length) {
+          $scope.oldHighlightlist = $scope.Highlightlist;
+        }
+
+        angular.forEach($scope.Highlightlist, function (item, index) {
+          if (item.matchName == $scope.oldHighlightlist[index].matchName) {
+            if (
+              item.runner1Back != $scope.oldHighlightlist[index].runner1Back
+            ) {
+              $("#hback1_" + index).addClass("spark");
+            }
+            if (
+              item.runner1Lay != $scope.oldHighlightlist[index].runner1Lay
+            ) {
+              $("#hlay1_" + index).addClass("spark");
+            }
+            if (
+              item.runner2Back != $scope.oldHighlightlist[index].runner2Back
+            ) {
+              $("#hback2_" + index).addClass("spark");
+            }
+            if (
+              item.runner2Lay != $scope.oldHighlightlist[index].runner2Lay
+            ) {
+              $("#hlay2_" + index).addClass("spark");
+            }
+            if (
+              item.runner3Back != $scope.oldHighlightlist[index].runner3Back
+            ) {
+              $("#hback3_" + index).addClass("spark");
+            }
+            if (
+              item.runner3Lay != $scope.oldHighlightlist[index].runner3Lay
+            ) {
+              $("#hlay3_" + index).addClass("spark");
+            }
+          }
+        });
+        $scope.oldHighlightlist = $scope.Highlightlist;
+
+        // console.log($scope.cricket)
+        // },
+        //   function myError(error) {
+        //     console.log("error")
+        //     $scope.showtennisinter = false;
+        //     if (error.status == 401) {
+        //       // $.removeCookie("authtoken");
+        //       // window.location.href="index.html"
+        //     }
+        //   }
+        // );
+      } else {
+        $scope.showtennisinter = false;
+        // console.log(response);
+        // $scope.cricket = tennisMocData.data;
 
         $scope.Highlightlist = $rootScope.highlightwisedata(2);
         if ($scope.Highlightlist.length < 1) {
